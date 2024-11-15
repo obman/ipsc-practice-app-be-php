bacd7ef5b6f7e35e9836b7b7bfd9a006c79213e9
denis.music <deniss9oo@gmail.com>
2024-11-15 19:03:34 +0100

    Renamed table name and file name

--- src/database/migrations/2024_11_15_165413_create_role_user_table.php
+++ src/database/migrations/2024_11_15_165413_create_role_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_roles', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreignId('role_id')->references('id')->on('roles')->onDelete('cascade');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('role_user');
     }
 };
