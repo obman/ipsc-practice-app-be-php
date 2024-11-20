2304abc44cfa3dd3f120fb7c1617806f5f9c810f
denismusic <denis.music@3appes.com>
2024-11-20 11:50:49 +0100

    Refactored routes, added logout route

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,13 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\ApiAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('/login', \App\Http\Controllers\Auth\API\UserLoginController::class)
-        ->name('login');
+    Route::post('/login', [ApiAuthController::class, 'login']);
 
-    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-        return $request->user();
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::post('/logout', [ApiAuthController::class, 'logout']);
+
+        // Test
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
     });
 });
