4d7e91039e96f1bc750d11143826a1d7c52086ae
denis.music <deniss9oo@gmail.com>
2024-11-23 19:50:17 +0100

    Implemented new user register method

--- src/app/Http/Controllers/Auth/ApiAuthController.php
+++ src/app/Http/Controllers/Auth/ApiAuthController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterUserRequest;
 use App\Http\Resources\UserResource;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class ApiAuthController extends Controller
 {
@@ -18,8 +21,23 @@ class ApiAuthController extends Controller
 
     // need new request class
     // where would be logic for creating new user?
-    public function register()
-    {}
+    public function register(RegisterUserRequest $request): UserResource
+    {
+        // this needs to move in a service class
+        $user = \App\Models\User::create([
+            'email' => $request->email,
+            'username' => $request->username,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'password' => $request->password,
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return new UserResource($user);
+    }
 
     public function logout(Request $request): Response
     {
