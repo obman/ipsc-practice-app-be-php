479ba7d64e8a7b400e82b1ce104bcf536bc16b9c
denis.music <deniss9oo@gmail.com>
2024-11-23 19:49:59 +0100

    Currently commented username muttator

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -70,7 +70,7 @@ class User extends Authenticatable
     {
         return Attribute::make(
             get: fn () => strtolower($this->first_name . $this->last_name),
-            set: fn () => strtolower($this->first_name . $this->last_name),
+            //set: fn () => strtolower($this->first_name . $this->last_name),
         );
     }
 
