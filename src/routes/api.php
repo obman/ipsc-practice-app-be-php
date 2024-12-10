b55c196770fc2a9695dcf8fd04fac87da60d38a1
denis.music <deniss9oo@gmail.com>
2024-12-10 21:43:54 +0100

    Changed controllers

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,16 +1,15 @@
 <?php
 
-use App\Http\Controllers\Auth\ApiAuthController;
+use App\Http\Controllers\Auth\Api\UserLoginController;
+use App\Http\Controllers\Auth\Api\UserRegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('/login', [ApiAuthController::class, 'login']);
-    Route::post('/register', [ApiAuthController::class, 'register']);
+    Route::post('/login', UserLoginController::class);
+    Route::post('/register', UserRegisterController::class);
 
     Route::middleware(['auth:sanctum'])->group(function () {
-        Route::post('/logout', [ApiAuthController::class, 'logout']);
-
         // Test
         Route::get('/user', function (Request $request) {
             return $request->user();
